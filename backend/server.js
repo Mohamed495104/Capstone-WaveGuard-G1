d1ab29b838b1275c3ad596897422d8fd3506657f
Mohamed495104 <mdijas555@gmail.com>
2025-10-06 19:26:46 -0400

    Updated Server port

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,5 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
